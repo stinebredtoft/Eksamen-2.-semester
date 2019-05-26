68bc531ab4c2aebedc263121c2aa24e68c44599c
Rasmus Gali Kristensen <boomse4you@gmail.com>
2019-05-26 21:25:02 +0200

    Update mitjs.js

--- mitjs.js
+++ mitjs.js
@@ -31,35 +31,3 @@ $(document).ready(function () {
 })
 
 /* Animering */
-var animateHTML = function () {
-    var elems;
-    var windowHeight;
-
-    function init() {
-        elems = document.querySelectorAll('.gemt');
-        windowHeight = window.innerHeight;
-        addEventHandlers();
-        checkPosition();
-    }
-
-    function addEventHandlers() {
-        window.addEventListener('scroll', checkPosition);
-        window.addEventListener('resize', init);
-    }
-
-    function checkPosition() {
-        for (var i = 0; i < elems.length; i++) {
-            var positionFromTop = elems[i].getBoundingClientRect().top;
-            if (positionFromTop - windowHeight <= 0) {
-                elems[i].className = elems[i].className.replace(
-                    'gemt',
-                    'optegn-svampen1'
-                );
-            }
-        }
-    }
-    return {
-        init: init
-    };
-};
-animateHTML().init();
